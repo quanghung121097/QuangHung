7cf0597a5c60e1b66b0da9a81e5797982976d6f3
quanghung121097 <49027988+quanghung121097@users.noreply.github.com>
2019-05-03 20:00:50 +0700

    Update 8.1.js

--- btvn/bai8/js/8.1.js
+++ btvn/bai8/js/8.1.js
@@ -1,8 +1,13 @@
-var i;
-alert('cac so chia het cho 3 va 7 trong [1;100] la: ')
+var i,a=0;
+document.write('cac so chia het cho 3 Hoac 7 trong [1;100] la: ')
+document.write('<br/>')
 for (var i = 1; i <=100; i++) {
-	if( i % 3 == 0 && i % 7 ==0)
+	if( i % 3 == 0 || i % 7 ==0)
 	{
-		alert(i)
+		document.write(i + ' - ');
+		a++
 	}
+
 }
+document.write('<br/>')
+document.write('co '+a+' so chia het cho 3 hoac 7')
