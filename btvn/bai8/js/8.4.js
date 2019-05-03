f2399a71358c4b99bc906bdadb0e1bd4754c9e53
quanghung121097 <49027988+quanghung121097@users.noreply.github.com>
2019-05-03 21:02:11 +0700

    Update 8.4.js

--- btvn/bai8/js/8.4.js
+++ btvn/bai8/js/8.4.js
@@ -1,16 +1,11 @@
 var n=prompt('nhap vao n');
 var m=prompt('nhap vao m');
 var k=prompt('nhap vao k');
+var i;
 var s=0;
-
-for (var i=n; i <= m; i++) {
-	if ((parseInt(i) % parseInt(k)) == 0) {
-		s=parseInt(s)+parseInt(i);
+for (i = n; i <= m; i++) {
+	if (i%k==0) {
+		s+=+i;
 	}
 }
-if (s==0) {
-	alert('khong co so nao trong khoang '+n+' den '+m+' chia het cho '+k)
-}
-else {
-	alert('tong la: '+s)
-}
+alert('tong cac so la: '+s)
